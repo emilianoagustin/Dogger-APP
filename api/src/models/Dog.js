8129c48296fb1eee1b2ac2a2d7849ab44c5b3890
emilianoagustin <pintos.emiliano@gmail.com>
2021-06-05 00:05:04 -0300

    Added DB tables

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -9,4 +9,34 @@ module.exports = (sequelize) => {
       allowNull: false,
     },
   });
+  sequelize.define('breed', {
+    ID: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lifeSpan: {
+      type: DataTypes.STRING
+    }
+  });
+  sequelize.define('temperament', {
+    ID: {
+      type: DataTypes.INTEGER
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  })
 };
