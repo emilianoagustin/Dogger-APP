02637f0c9c3a9597792ac0959ad623b9e12a4546
emilianoagustin <pintos.emiliano@gmail.com>
2021-06-07 08:53:08 -0300

    added routes to index.js, created dogs.js and temperament.js routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const dogs = require('./dogs');
+const temperament = require('./temperament');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use('/dogs', dogs);
+router.use('/temperament', temperament);
 
 module.exports = router;
