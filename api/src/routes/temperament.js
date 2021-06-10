0ac7fab751448b5eeecc881bb88c970688c4e03d
emilianoagustin <pintos.emiliano@gmail.com>
2021-06-10 14:44:59 -0300

    removed/moved dogs and temperaments js files to GET and POST folders

--- api/src/routes/temperament.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const getTemperament = require('../controllers/temperament');
-
-router.get('/', getTemperament);
-
-module.exports = router;
