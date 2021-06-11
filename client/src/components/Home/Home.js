0f1a5b810c1095b559162bdf43c89bac6c0d1f66
emilianoagustin <pintos.emiliano@gmail.com>
2021-06-11 12:53:50 -0300

    added dispatch hook to home component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import { React, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getDogs } from '../../actions/actions';
 import Breed from '../Breed/Breed';
 import FilterInput from '../FilterInput/FilterInput';
 import OrderInput from '../OrderInput/OrderInput';
@@ -6,6 +8,10 @@ import SearchInput from '../SearchInput/SearchInput';
 import Pagination from '../Pagination/Pagination';
 
 function Home() {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getDogs())
+    }, [])
     return (
         <div>
             <SearchInput/>
