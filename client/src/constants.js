1a40eac09982c4409ad83f9ece82289cd7941cd2
emilianoagustin <pintos.emiliano@gmail.com>
2021-06-11 15:14:24 -0300

    changed url port in constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
-export const DOG_URL = 'http://localhost:3000/dogs/';
-export const CREATE_DOG_URL = 'http://localhost:3000/dog';
-export const TEMPERAMENT_URL = 'http://localhost:3000/temperament';
+export const DOG_URL = 'http://localhost:3001/dogs';
+export const CREATE_DOG_URL = 'http://localhost:3001/dog';
+export const TEMPERAMENT_URL = 'http://localhost:3001/temperament';
