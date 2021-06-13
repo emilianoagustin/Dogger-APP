870505ba2d8a05eff23aa00df961ae835d232481
emilianoagustin <pintos.emiliano@gmail.com>
2021-06-12 21:00:45 -0300

    added search input functionality

--- client/src/components/SearchInput/SearchInput.js
+++ client/src/components/SearchInput/SearchInput.js
@@ -1,23 +1,23 @@
 import { React } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { searchDogs } from '../../actions/actions';
+import { inputState, searchDogs } from '../../actions/actions';
 
 function SearchInput() {
     
     const dispatch = useDispatch();
-    const searchDog = useSelector(state => state.searchDogs);
-    console.log(searchDog);
-    const handleSearchSubmit = (e) => {
-        e.preventDefault();
-        dispatch(searchDogs(e.target.value));
+    const value = useSelector(state => state.inputValue);
+    const onChange = (e) => {
+        dispatch(inputState(e.target.value));
+    }
+    const handleSearch = () => {
+        dispatch(searchDogs(value));
+        dispatch(inputState(''));
     }
 
     return (
         <div>
-            <form onSubmit={(e) => handleSearchSubmit(e)}>
-                <input type='text' placeholder='search breed' value=''/>
-                <button type='submit'>search</button>
-            </form>
+                <input type='text' placeholder='search breed' value={value} onChange={onChange}/>
+                <button type='button' onClick={handleSearch}>search</button>
         </div>
     )
 }
