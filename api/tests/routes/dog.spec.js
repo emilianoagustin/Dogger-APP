3585cbaad11d22ee4a8292a11eb0e4bc1c1eebe1
emilianoagustin <pintos.emiliano@gmail.com>
2021-06-18 17:09:52 -0300

    added models y routes tests

--- api/tests/routes/dog.spec.js
+++ api/tests/routes/dog.spec.js
@@ -6,19 +6,60 @@ const { Dog, conn } = require('../../src/db.js');
 
 const agent = session(app);
 const dog = {
-  name: 'Pug',
+  name: 'Henry_Dog',
+  height: '15',
+  weight: '8'
 };
 
-describe('Videogame routes', () => {
+describe('Dogs routes', () => {
   before(() => conn.authenticate()
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   }));
   beforeEach(() => Dog.sync({ force: true })
     .then(() => Dog.create(dog)));
+
   describe('GET /dogs', () => {
-    it('should get 200', () =>
-      agent.get('/dogs').expect(200)
-    );
+    it('should get 200', () => {
+      return agent.get('/dogs').expect(200);
+    });
+    it('espera que responda con un json', () => {
+      return agent.get('/dogs')
+        .expect('Content-Type', /json/);
+    });
+  });
+  describe('GET /dogs?name=', () => {
+    it('responde con 404 cuando el Dog no existe', () => {
+      return agent.get('/dogs?name=wrongdogname')
+        .expect(404);
+    });
+    it('responde con 200 cuando el Dog existe', () => {
+      return agent.get('/dogs?name=terrier')
+        .expect(200);
+    });
+    it('espera que responda con un json', () => {
+      return agent.get('/dogs?name=terrier')
+        .expect('Content-Type', /json/);
+    });
+  });
+  describe('GET /dogs/:id', () => {
+    it('responde con 404 cuando el Dog no existe', () => {
+      return agent.get('/999')
+        .expect(404);
+    });
+    it('responde con 200 cuando el Dog existe', () => {
+      return agent.get('/200')
+        .expect(200);
+    });
+  });
+  describe('POST /dog', () => {
+    it('responde con 400 cuando faltan campos requeridos', () => {
+      return agent.post('/dog')
+        .send({
+          weight: '23'
+        })
+        .expect(400);
+    });
   });
 });
+
