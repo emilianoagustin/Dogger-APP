e6ed5b02cf6d4e4284de3192511ba0743dc6548d
emilianoagustin <pintos.emiliano@gmail.com>
2021-06-30 15:04:37 -0300

    deleted styles from app.css, added effects to search, filter and more details buttons

--- client/src/components/Home/SearchBox/SearchBox.js
+++ client/src/components/Home/SearchBox/SearchBox.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import { MdSearch } from 'react-icons/md';
-import { IconContext } from "react-icons";
 import './SearchBox.css';
 
 function SearchBox({ onChange, onClick, value }) {
 
     return (
         <div className='searchbox-container'>
-                <input className='searchbox-input' type='text' placeholder='search breed' name='name' value={value} onChange={(e) => onChange(e)}/>
-                <button className='searchbox-button' type='button' onClick={onClick}>search
-                <IconContext.Provider value={{ className:'search-icon'}}>
-                <MdSearch />
-                </IconContext.Provider>
-                </button>
+                <input className='searchbox-input' type='text' placeholder='search your dog...' name='name' value={value} onChange={(e) => onChange(e)}/>
+                <span className='searchbox-button' onClick={onClick}>
+                    <MdSearch />
+                </span>
         </div>
     )
 }
