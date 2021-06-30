47f5003628192f2ab310d17feefcc712edb5db40
emilianoagustin <pintos.emiliano@gmail.com>
2021-06-30 01:03:35 -0300

    changed app background, dog cards and searchbox button

--- client/src/components/Home/SearchBox/SearchBox.js
+++ client/src/components/Home/SearchBox/SearchBox.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { MdSearch } from 'react-icons/md';
+import { IconContext } from "react-icons";
 import './SearchBox.css';
 
 function SearchBox({ onChange, onClick, value }) {
@@ -6,7 +8,11 @@ function SearchBox({ onChange, onClick, value }) {
     return (
         <div className='searchbox-container'>
                 <input className='searchbox-input' type='text' placeholder='search breed' name='name' value={value} onChange={(e) => onChange(e)}/>
-                <button className='searchbox-button' type='button' onClick={onClick}>search</button>
+                <button className='searchbox-button' type='button' onClick={onClick}>search
+                <IconContext.Provider value={{ className:'search-icon'}}>
+                <MdSearch />
+                </IconContext.Provider>
+                </button>
         </div>
     )
 }
