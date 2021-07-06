69004b53f6178a6f9ad5a1723701a5a1adc2f1a8
emilianoagustin <pintos.emiliano@gmail.com>
2021-07-06 01:22:55 -0300

    added dog.gif and created the loading component

---
+++ client/src/components/Loading/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Loading.css';
+
+function Loading() {
+    return (
+        <div className='loading-container'>
+            <span className='loading-img'></span>
+            <span>Loading...</span> 
+        </div>
+    )
+}
+
+export default Loading
