62136979a2c189fceeb216c89ebd91a4c0349c6a
emilianoagustin <pintos.emiliano@gmail.com>
2021-07-28 01:10:29 -0300

    testing deploy

--- api/src/app.js
+++ api/src/app.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const {
+  DB_HOST, DB_PORT
+} = process.env;
 
 require('./db.js');
 
@@ -14,7 +18,7 @@ server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', `https://${DB_HOST}:${DB_PORT}` || 'http://localhost:3000'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
