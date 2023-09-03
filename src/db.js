12969509736d3da777bd1c2d620c38a4afe8df61
emilianoagustin <pintos.emiliano@gmail.com>
2023-09-03 02:17:22 -0300

    new db changes

--- src/db.js
+++ src/db.js
@@ -2,26 +2,32 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD } = process.env;
+const { DB_USER, DB_PASSWORD, POSTGRES_URL } = process.env;
 const axios = require('axios');
 const { URL } = require('./constants/constants');
 
-const sequelize = new Sequelize(
-  "postgres://"+DB_USER+":"+DB_PASSWORD+"@ec2-3-231-194-96.compute-1.amazonaws.com:5432/d4d1e9v90ut59c",
-  {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialect: 'postgres',
-  ssl: true,
-  protocol: 'postgres',
-  dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  },
-);
+const sequelize = new Sequelize(POSTGRES_URL,{dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },});
+// const sequelize = new Sequelize(
+//   "postgres://"+DB_USER+":"+DB_PASSWORD+"@ec2-3-231-194-96.compute-1.amazonaws.com:5432/d4d1e9v90ut59c",
+//   {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//   dialect: 'postgres',
+//   ssl: true,
+//   protocol: 'postgres',
+//   dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false,
+//       },
+//     },
+//   },
+// );
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
